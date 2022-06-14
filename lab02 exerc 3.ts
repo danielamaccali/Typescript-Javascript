96f268f498daeea55610e08c464e63902e3d8581
Daniela Maccali <daniela.maccali@gmail.com>
2022-06-14 18:56:05 -0300

    Exercicio 3 - item B

--- lab02 exerc 3.ts
+++ lab02 exerc 3.ts
@@ -70,9 +70,36 @@ class ClienteJuridico extends Cliente
     }
 }
 
+
+// implementação exercicio 3.a
+class CadastroClientes
+{
+    private clienteF: Map<ClienteFisico, number>; //seria uma coleção neste formato?
+
+    constructor()
+    {
+        this.clienteF = new Map<ClienteFisico, number>;
+    }
+
+    adicionarCliente(cliente: ClienteFisico) : ClienteFisico //teste
+    {
+        
+    }
+}
+
 let cf1 = new ClienteFisico("Fulano de tal", 35, 2000)
 let cj1 = new ClienteJuridico("Empresa T1000", 200)
 
 console.log(`\n Cliente: ${cf1.getNome()} \n Idade: ${cf1.getIdade()} \n Salário: ${cf1.getSalario()} \n Mensalidade: ${cf1.getMensalidade()}`);
 
 console.log(`\n Cliente: ${cj1.getNome()} \n Mensalidade: ${cj1.getMensalidade()}`);
+
+
+// a) Implemente as respectivas classes. Para o cálculo do valor da mensalidade utilize as seguintes informações: 
+//Esta empresa de seguros cobra uma mensalidade correspondente a 10% do salário se o cliente tem menos de 60 anos e 
+//15% se o cliente tem 60 anos ou mais, no caso de clientes que são pessoa física. 
+//Para pessoas jurídicas é cobrada uma mensalidade variável, calculada através de negociações com o cliente.
+
+// b) Implemente uma classe CadastroClientes para armazenar o cadastro de clientes da empresa de seguros. 
+//Apresente métodos para adicionar um cliente ao cadastro e retornar uma string contendo a listagem de todos os clientes, 
+//mostrando seu nome e o valor da mensalidade a ser paga. Utilize uma coleção em memória.
